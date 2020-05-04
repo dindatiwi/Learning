ba4cb7be0f6efa1572e0b42d15ec4d8146b9be29
Adinda Pratiwi Prameswari <44132935+dindatiwi@users.noreply.github.com>
2020-05-04 19:23:26 +0700

    add dog-years js
    simple console about dog years with javascript from codeacademy

---
+++ index.js
@@ -0,0 +1,12 @@
+const myAge = 20
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+console.log(`${laterYears} , ${earlyYears}`)
+myAgeInDogYears = laterYears + earlyYears
+
+
+myName = 'DINDUT'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
